77776dd457482aa760c16a8d4450b83527e9383d
Myeongzip <shayne0914@skuniv.ac.kr>
2023-10-24 22:33:14 +0900

    first init

---
+++ recommend.js
@@ -0,0 +1,14 @@
+console.log('hi!')
+
+// 목록을 뽑아내는 함수, 반복문을 이용해서 리스트 정렬
+function read_list() {
+    for (let i = 0; i < response_json.images.length; i++) {
+        i
+    }
+}
+
+funcion 
+
+// 거리순
+// 리뷰 많은 순
+// 평점 순
